e1c2126fad527716ccc13f7ef7392abae77a58ca
marccsaada <marc.abou-saada@etud.polymtl.ca>
2026-09-06 10:27:23 -0400

    Change month names to French
    Signed-off-by: marccsaada <marc.abou-saada@etud.polymtl.ca>

--- src/components/month-year-select.tsx
+++ src/components/month-year-select.tsx
@@ -9,18 +9,18 @@ import {
 } from '@/components/ui/select'
 
 const MONTH_NAMES = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
+  'janvier',
+  'février',
+  'mars',
+  'avril',
+  'mai',
+  'juin',
+  'juillet',
+  'août',
+  'septembre',
+  'octobre',
+  'novembre',
+  'décembre',
 ]
 
 export interface MonthYear {
