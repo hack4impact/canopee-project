31cb19aee5ed86c59134ba74008e872332feeb5d
Anis Benabdallah <anis.benabdallah1@icloud.com>
2026-09-19 17:56:55 -0400

    fix(ios): stop the webview from pinch zooming

--- ios/App/App/MainViewController.swift
+++ ios/App/App/MainViewController.swift
@@ -5,4 +5,19 @@ class MainViewController: CAPBridgeViewController {
     override open func capacitorDidLoad() {
         bridge?.registerPluginInstance(PatrolActivityPlugin())
     }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        guard let scrollView = webView?.scrollView else {
+            return
+        }
+
+        scrollView.pinchGestureRecognizer?.isEnabled = false
+        scrollView.bouncesZoom = false
+
+        if scrollView.zoomScale != 1 {
+            scrollView.setZoomScale(1, animated: false)
+        }
+    }
 }
